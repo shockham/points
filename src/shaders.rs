3c232ee9efe4e69665f69c7aba1b0abe93f18e75
shockham <samuel.hockham@gmail.com>
2017-11-05 10:21:26 +0000

    Initial distance fall off on point density

--- src/shaders.rs
+++ src/shaders.rs
@@ -30,19 +30,25 @@ mod points {
         out vec3 tc_normal[];
         out vec2 tc_texture[];
 
+        uniform vec3 cam_pos;
         const float outer = 1.0;
-        const float inner = 3.0;
+        const float inner_range = 200.0;
 
         void main() {
             tc_normal[gl_InvocationID] = v_normal[gl_InvocationID];
             tc_texture[gl_InvocationID] = v_texture[gl_InvocationID];
             gl_out[gl_InvocationID].gl_Position = gl_in[gl_InvocationID].gl_Position;
 
+            float dist = abs(distance(cam_pos, vec3(gl_in[gl_InvocationID].gl_Position)));
+            float dist_t =
+                (step(dist, inner_range) * (((inner_range - dist) / inner_range) * 10.0));
+
             float d_01 = distance(gl_out[0].gl_Position, gl_out[1].gl_Position);
             float d_02 = distance(gl_out[0].gl_Position, gl_out[2].gl_Position);
             float d_12 = distance(gl_out[1].gl_Position, gl_out[2].gl_Position);
+            float spacing_t = max(d_01, max(d_02, d_12)) / 0.22;
 
-            float inner_t = max(d_01, max(d_02, d_12)) / 0.22;
+            float inner_t = (dist_t + spacing_t) / 2.0;
 
             gl_TessLevelOuter[0] = outer;
             gl_TessLevelOuter[1] = outer;
